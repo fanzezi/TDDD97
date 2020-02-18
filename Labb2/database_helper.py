be16c48a8004aeb7d0c491dc80561dabbf8bfc5d
Emma Algotsson <emmaalgotsson@win01172.ad.liu.se>
2020-02-18 10:15:43 +0100

    Updated lab 2

--- Labb2/database_helper.py
+++ Labb2/database_helper.py
@@ -6,7 +6,56 @@ def connect_db():
     return sqlite3.connect("database.db")
 
 def get_db():
+    db = g# find_user(), remove_user(), create_post()
+import sqlite3
+from flask import g
+
+#connect to database
+def connect_db():
+    return sqlite3.connect("database.db")
+
+#get database
+def get_db():
     db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_db()
+    return db
+
+#get email from token
+def tokenToEmail(token):
+    c = get_db()
+    result = c.execute('SELECT email FROM loggedInUser WHERE token = ?', (token,))
+    row = result.fetchone()
+    return row[0]
+
+# get user
+def copyUser(email):
+    c = get_db()
+    result = c.execute('SELECT * FROM users WHERE email = ?', (email,))
+    user = result.fetchall()
+    return user
+
+# find user
+def find_user(email, password):
+    db = get_db()
+    user = (email, password)
+    if user:
+        return True
+    else:
+        return False
+
+def create_post():
+    #do stuff
+    return ""
+
+def store_message():
+    #do stuff
+    return ""
+
+def remove_user():
+    # do stuff
+    return ""
+etattr(g, 'db', None)
     if db is None:
         db = g.db = connect_db()
     return db
--- Labb2/server.py
+++ Labb2/server.py
@@ -7,20 +7,13 @@ import json
 #implemented using Python and Flask
 # signup singin
 
-# token = generate_token()
-#         res = database_helper.save_token(email, token)
-#         if res:
-#             return json.dumps({'success':True, 'message':"You successfully signed in", 'data':token})
-#         else:
-#             return json.dumps({'success':False, 'message':"Sign in was unsuccessful", 'data':""})
-
-
 
 @app.route("/sign_in", methods=['GET', 'POST'])
 def sign_in():
     email = request.json['email']
     password = request.json['password']
 
+    #if user exists
     if database_helper.find_user(email,password):
         token = generate_token()
         #save token
@@ -44,9 +37,11 @@ def sign_up():
     city = request.json['city']
     country = request.json['country']
 
+    # if somethings missing:
+    # return json.dumps({'success': False, 'message': "Form data missing or incorrect type."})
+
     return json.dumps({'success': True, 'message': "Successfully created a new user."})
 
-    return json.dumps({'success': False, 'message': "Form data missing or incorrect type."})
 
 
 @app.route("/change-password", methods=['POST'])
@@ -56,19 +51,71 @@ def change_password():
     oldPassword = request.json['oldPassword']
     newPassword = request.json['newPassword']
 
+    # if old password equal to new password
     if oldPassword == newPassword:
         return json.dumps({'success': False, 'message': "Can't use the same password"})
+
+    #if password length is less than 4 characters
+    if len(newPassword)< 4:
+        return json.dumps({'success': False, 'message': "Password must be at least 3 characters long"})
+
+    #get email from token
+    email = database_helper.tokenToEmail(token)
+
+    # if user not logged in
+    if not email:
+        return json.dumps({'success': False, 'message': "You are not logged in!"})
+
+    # if wrong password
+    if not database_helper.find_user(email, oldPassword):
+        return json.dumps({'success': False, 'message': "Wrong password"})
+
+    #return password successfully changed
     return json.dumps({'success': True, 'message': "Password changed"})
-    return json.dumps({'success': False, 'message': "Wrong password"})
-    return json.dumps({'success': False, 'message': "You are not logged in!"})
 
 def get_user_data_by_token()
 
 
 def get_user_data_by_email()
 
-def get_user_messages_by_token()
+def get_user_messages_by_token():
+    token = request.json['token']
+    email = database_helper.tokenToEmail(token)
 
-def get_user_messages_by_email()
+@app.route("/get-user-messages-by-email", methods=['GET'])
+def get_user_messages_by_email():
+    token = request.json['token']
+    email = request.json['email']
 
-def post_message()
+    #get user
+    match = database_helper.copyUser(email)
+
+    # if user do not exist
+    if not match:
+        return json.dumps({'success': False, 'message': "No such user"})
+
+    #if user is not signed in
+    if not database_helper.tokenToEmail(token):
+        return json.dumps({'success': False, 'message': "You are not signed in"})
+
+    #return user messages
+    return json.dumps({'success': True, 'message': "User messages retrived", 'data': match})
+
+#post message
+@app.route("/post-message", methods=['POST'])
+def post_message():
+    token = request.json['token']
+    message = request.json['message']
+    toEmail = request.json['email']
+
+    fromEmail = database_helper.tokenToEmail(token)
+
+    if not fromEmail:
+        return json.dumps({'success': False, 'message': "You are not signed in"})
+
+    if not database_helper.find_user(toEmail):
+        return json.dumps({'success': False, 'message': "No such user"})
+
+    #result = database_helper.store_message(toEmail, fromEmail, message)
+    # if result
+    return json.dumps({'success': True, 'message': "Message posted"})
